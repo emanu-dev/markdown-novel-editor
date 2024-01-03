7641fc251c086e077a81214443cf554f21649d4f
Emanuel Prado <emanuel.prado.dev@gmail.com>
2024-01-02 21:09:34 -0300

    wip: trying out some markdown editors

--- frontend-new/src/components/Editor.tsx
+++ frontend-new/src/components/Editor.tsx
@@ -1,4 +1,9 @@
-import { createSignal } from 'solid-js';
+import { createSignal, onCleanup, onMount } from 'solid-js';
+import { defaultValueCtx, Editor as MilkdownEditor, rootCtx } from '@milkdown/core';
+import { commonmark } from '@milkdown/preset-commonmark';
+import { nord } from '@milkdown/theme-nord';
+import { listener, listenerCtx } from '@milkdown/plugin-listener';
+
 
 interface IProps {
   content: string;
@@ -11,25 +16,35 @@ const Editor = (props: IProps) => {
     onSave
   } = props;
 
-  const [content, setContent] = createSignal(contentByProp);
+  let ref: HTMLDivElement | undefined;
+  let editor: MilkdownEditor;
+  let markdownContent: string = contentByProp;
+
+  onMount(async () => {
+    editor = await MilkdownEditor.make()
+      .config((ctx) => {
+        ctx.set(rootCtx, ref);
+        ctx.get(listenerCtx).markdownUpdated((ctx, markdown, prevMarkdown) => {
+          markdownContent = markdown;
+        });
+      })
+      .config(nord)
+      .use(commonmark)
+      .use(listener)
+      .create();
+  });
+
+  onCleanup(() => {
+    editor.destroy();
+  });
 
   const handleSave = () => {
-    onSave(content());
+    onSave(markdownContent);
   };
-
-  const handleOnInput = (e: InputEvent & {
-    currentTarget: HTMLTextAreaElement;
-    target: HTMLTextAreaElement;
-  }) => {
-    setContent(e.currentTarget.value);
-  }
 
   return (
     <div>
-      <textarea
-        value={content()}
-        onInput={handleOnInput}
-      />
+      <div ref={ref} />
       <button onClick={handleSave}>Save</button>
     </div>
   );
